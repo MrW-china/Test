86e87654a932152da6ecfe3a1afa6693a9ccf492
Mr_W <1048995287@qq.com>
2021-03-08 16:53:58 +0800

    二叉树中和为某一值的路径(解决)

--- src/main/java/cn/dayTest/year_2021/tree/medium/month_03/day_08/pathSum/Solution.java
+++ src/main/java/cn/dayTest/year_2021/tree/medium/month_03/day_08/pathSum/Solution.java
@@ -1,16 +1,25 @@
 package cn.dayTest.year_2021.tree.medium.month_03.day_08.pathSum;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
+    List<List<Integer>> lists =new LinkedList<>();
+    LinkedList<Integer> list =new LinkedList<>();
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-
-        return null;
+        dfs(root,sum);
+        return lists;
     }
 
-    public List<Integer> dfs(TreeNode root ,int sum ){
-
-
-        return null;
+    public void dfs(TreeNode root ,int sum ){
+        if (root==null) return;
+        int val = root.val;
+        list.add(val);
+        if (sum-val==0&&root.left==null&&root.right==null){
+            lists.add(new LinkedList<>(list));
+        }
+        dfs(root.left,sum-val);
+        dfs(root.right,sum-val);
+        list.removeLast();
     }
 }
