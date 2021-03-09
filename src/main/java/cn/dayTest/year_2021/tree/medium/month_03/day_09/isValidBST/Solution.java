25dc73544d96e30f7bdcaee1b6c8dcfb39fef551
Mr_W <1048995287@qq.com>
2021-03-09 14:23:32 +0800

    验证二叉搜索树

---
+++ src/main/java/cn/dayTest/year_2021/tree/medium/month_03/day_09/isValidBST/Solution.java
@@ -0,0 +1,23 @@
+package cn.dayTest.year_2021.tree.medium.month_03.day_09.isValidBST;
+
+public class Solution {
+
+    public static void main (String[] args) {
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println(maxValue);
+    }
+    public boolean isValidBST (TreeNode root) {
+//        if (root.left != null && root.left.val >= root.val) return false;
+//        if (root.right != null && root.right.val <= root.val) return false;
+//        return dfs(root.left, root.val) && dfs(root.right, root.val);
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//        return false;
+    }
+
+    public boolean dfs (TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val >= max || root.val <= min) return false;
+        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+
+    }
+}
