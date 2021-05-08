60e05896ab2d70dd514bc0c198c336f9dc1f30b1
Mr_W <1048995287@qq.com>
2021-05-08 10:46:41 +0800

    根据数字二进制下 1 的数目排序v2.0

--- src/main/java/cn/dayTest/year_2021/sort/month_04/day_29/sortByBits/SortByBits.java
+++ src/main/java/cn/dayTest/year_2021/sort/month_04/day_29/sortByBits/SortByBits.java
@@ -8,52 +8,73 @@ import java.util.List;
 
 public class SortByBits {
     public static void main (String[] args) {
-
-
-
-
-
+        int i=3;
+        System.out.println(i>>1+(i&1));
     }
 }
 
 class Solution {
-    public  int[] sortByBits(int[] arr) {
+    public int[] sortByBits (int[] arr) {
         //最大数10000，最小0
-        int[] bit=new int[10001];
+        int[] bit = new int[10001];
         /*将数组中的数赋值到list集合之中，
         且将二进制中‘1’的个数保存到相应位置的bit数组之中
         */
-        List<Integer> list=new ArrayList<>();
-        for (int x :arr){
+        List<Integer> list = new ArrayList<>();
+        for (int x : arr) {
             list.add(x);
-            bit[x]= get(x);
+            bit[x] = get(x);
         }
         Collections.sort(list, new Comparator<Integer>() {
             @Override
             public int compare (Integer x, Integer y) {
-                if (bit[x]!=bit[y]){
-                    return bit[x]-bit[y];
+                if (bit[x] != bit[y]) {
+                    return bit[x] - bit[y];
                 }
-                return x-y;
+                return x - y;
             }
         });
         for (int i = 0; i < list.size(); i++) {
-            arr[i]=list.get(i);
+            arr[i] = list.get(i);
         }
         return arr;
     }
 
     //计算数二进制中有几个'1';
-    public int get(int x){
-        int res =0;
-        while (x!=0){
-            res +=x%2;
-            x /=2;
+    public int get (int x) {
+        int res = 0;
+        while (x != 0) {
+            res += x % 2;
+            x /= 2;
         }
         return res;
     }
 }
 
-
+class Solution2 {
+    public int[] sortByBits (int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int x : arr) {
+            list.add(x);
+        }
+        int[] bit = new int[10001];
+        for (int i = 1; i <= 10000; i++) {
+            bit[i] = bit[i >> 1] + (i & 1);
+        }
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare (Integer x, Integer y) {
+                if (bit[x] != bit[y]) {
+                    return bit[x] - bit[y];
+                }
+                return x - y;
+            }
+        });
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
 
 
