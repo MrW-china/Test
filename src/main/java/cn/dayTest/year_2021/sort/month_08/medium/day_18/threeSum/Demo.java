f01e49a4478e512c20520656f3133e2f3d0999c7
MrW-china <1048995287@qq.com>
2021-10-26 09:52:12 +0800

    填充每个节点的下一个右侧节点指针

---
+++ src/main/java/cn/dayTest/year_2021/sort/month_08/medium/day_18/threeSum/Demo.java
@@ -0,0 +1,24 @@
+package cn.dayTest.year_2021.sort.month_08.medium.day_18.threeSum;
+
+
+import java.util.List;
+
+/*
+*  question: 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+*
+* */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+*  answer:
+* */
+class Solution{
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
+}
